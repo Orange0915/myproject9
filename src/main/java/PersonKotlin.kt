f34d8446f5ad4464a846c598d1076e6f70ab3029
Orange0915 <liu910915@gmail.com>
2022-06-10 12:55:54 +0800

    commit kotlin heiio world

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.will
+
+class PersonKotlin {
+    fun greeting() {
+        println("Hello kotlin")
+    }
+}
+
+//1st level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
